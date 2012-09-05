54154dae9d5b9fa675bbf113d99e2aab16202e13
Jeremy Hughes <jedahu@gmail.com>
2012-09-05 17:10:07 +1200

    pygments filter: use text lexer by default

--- asciidoc/filters/source/asciidoc_pygments_filter.py
+++ asciidoc/filters/source/asciidoc_pygments_filter.py
@@ -4,7 +4,12 @@ from pygments.lexers import get_lexer_by_name
 from os import linesep
 
 def filter(lines, encoding='utf-8', language='text', src_numbered=False, **attrs):
+  lexer = get_lexer_by_name('text', encoding=encoding)
+  try:
+    lexer = get_lexer_by_name(language, encoding=encoding)
+  except Exception:
+    pass
   return highlight(
       linesep.join(lines),
-      get_lexer_by_name(language, encoding=encoding),
+      lexer,
       HtmlFormatter(linenos=bool(src_numbered), encoding=encoding))
